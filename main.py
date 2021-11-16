bfbe407714eddd3ebc232f8a0a797680e9df12b7
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-11-17 03:08:36 +0700

    config.json support (#2)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from environment import get_from_dotenv
+from environment import *
 from mail import send_mail
 
 
@@ -20,14 +20,15 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', date
 
 class Base:
 
-    def __init__(self, day, session, verbose, sch=get_from_dotenv('schedule')):
+    def __init__(self, day, session, verbose, mode):
         options = Options()
         if not verbose: options.add_argument('headless')
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(options=options, service_log_path='NUL')
-        self.name_class = sch[day]['name'][session] if session is not None else sch[day]['name']
-        self.link_class = sch[day]['link'][session] if session is not None else sch[day]['link']
-        self.data_log = f'{datetime.now().strftime("%d %b")} - {self.name_class}'
+        self.sch, self.username, self.password = mode('schedule'), mode('username'), mode('password')
+        self.class_name = self.sch[day]['name'][session] if session is not None else self.sch[day]['name']
+        self.class_link = self.sch[day]['link'][session] if session is not None else self.sch[day]['link']
+        self.data_log = f'{datetime.now().strftime("%d %b")} - {self.class_name}'
 
     def visit(self, url):
         self.driver.get(url)
@@ -49,8 +50,8 @@ class Base:
 
 class Attend(Base):
 
-    def __init__(self, day, session, verbose):
-        super().__init__(day, session, verbose)
+    def __init__(self, day, session, verbose, mode):
+        super().__init__(day, session, verbose, mode)
 
         self.login_url = 'http://elearning.bsi.ac.id/login'
 
@@ -66,9 +67,9 @@ class Attend(Base):
             'presence_tab': '#myTable > tbody'
         }
 
-    def login(self, username=get_from_dotenv('username'), password=get_from_dotenv('password')):
-        self.input_keys(By.CSS_SELECTOR, self.login_locator['username_input'], username)
-        self.input_keys(By.CSS_SELECTOR, self.login_locator['password_input'], password)
+    def login(self):
+        self.input_keys(By.CSS_SELECTOR, self.login_locator['username_input'], self.username)
+        self.input_keys(By.CSS_SELECTOR, self.login_locator['password_input'], self.password)
         self.click(By.XPATH, self.login_locator['login_button'])
 
     def get_button_status(self):
@@ -83,40 +84,40 @@ class Attend(Base):
         status = [data.split()[1] for data in raw.text.split('\n')][-1]
         return status
 
-def attend_class(sch=get_from_dotenv('schedule')):
+def attend_class(mode=get_from_config, mail=False):
+    sch = mode('schedule')
     today = datetime.now().strftime('%A')
 
     if today not in sch:
         return logging.info('No class today.')
 
-    class_schedule = sch[today]['time']
-
-    if not any(isinstance(nest, list) for nest in class_schedule):
-        start, end = class_schedule
-
-    current_time = datetime.now().strftime('%H:%M')
     no_class = True
 
-    if start:
-        if start <= current_time < end:
-            job(today)
-            no_class = False
-    else:
+    class_schedule = sch[today]['time']
+    current_time = datetime.now().strftime('%H:%M')
+
+    if any(isinstance(nest, list) for nest in class_schedule):
         for session in range(len(class_schedule)):
             start, end = class_schedule[session]
             if start <= current_time < end:
-                job(today, session)
+                job(today, session, mode, mail)
                 no_class = False
                 break
+    else:
+        start, end = class_schedule
+        if start <= current_time < end:
+            job(today, mode, mail)
+            no_class = False
 
     if no_class:
-        logging.info(f'Not in a class schedule now. Will start once class is open.')
+        logging.info(f'Not in a class schedule now.')
 
-def job(day, session=None, verbose=False):
+def job(day, session=None, verbose=False, mode=get_from_dotenv, mail=True):
     timer = time.time()
-    obj = Attend(day, session, verbose)
 
-    logging.info(f'Start of program - {obj.name_class}')
+    obj = Attend(day, session, verbose, mode)
+
+    logging.info(f'Start of program - {obj.class_name}')
     logging.info('Attempting to login...')
 
     obj.visit(obj.login_url)
@@ -127,14 +128,14 @@ def job(day, session=None, verbose=False):
     user = obj.driver.find_element(By.CLASS_NAME, 'user-name')
     name = obj.driver.find_element(By.ID, 'eMail').get_attribute('value')
 
-    if user.text != get_from_dotenv('username'):
+    if user.text != mode('username'):
         logging.info('Either your username or password is wrong. Quitting...')
         quit()
 
     logging.info(f'Success. Logged in as {name.title()}!')
-    logging.info(f'Attempting to attend {obj.name_class}')
+    logging.info(f'Attempting to attend {obj.class_name}')
 
-    obj.visit(obj.link_class)
+    obj.visit(obj.class_link)
 
     time.sleep(3)
 
@@ -154,7 +155,7 @@ def job(day, session=None, verbose=False):
         logging.info(f'Button Status: {obj.get_button_status()}')
         logging.info('Attempting to push the attendance button...')
         if obj.get_button_status() == 'Belum Mulai':
-            logging.info(f'Waiting 1 min. The class hasn\t stated yet.')
+            logging.info(f'Waiting 1 min. The class hasn\'t stated yet.')
             retry = True
         else:
             obj.click(By.XPATH, obj.attend_locator['ready'])
@@ -165,8 +166,6 @@ def job(day, session=None, verbose=False):
 
     if obj.get_button_status() == 'Kirim' and obj.get_tab_status() == 'Hadir':
         logging.info('Automation sucess.')
-    elif obj.get_button_status() == 'Sudah Selesai':
-        logging.info('Already automated or attended.')
     elif pending:
         logging.info('No class today.')
     else:
@@ -174,20 +173,21 @@ def job(day, session=None, verbose=False):
 
     obj.driver.close()
 
-    send_mail(f'Absen {obj.name_class}', f'logs\\{datetime.now().strftime("%d %b")}.txt', img_name)
+    if mail:
+        send_mail(f'Absen {obj.class_name}', f'logs\\{datetime.now().strftime("%d %b")}.txt', img_name, mode)
 
     logging.info(f'Automation completed in {time.time() - timer:.0f} seconds')
 
 
 def main():
-    attend_class()
-
+    attend_class(mode=get_from_config, mail=False)
+    
     '''
     schedule.every().monday.at('07:00').do(job, 'Monday')
     schedule.every().tuesday.at('07:00').do(job, 'Tuesday')
     schedule.every().wednesday.at('07:00').do(job, 'Wednesday')
     schedule.every().thursday.at('07:00').do(job, 'Thursday', 0)
-    schedule.every().thursday.at('09:00').do(job, 'Thursday', 1)
+    schedule.every().thursday.at('10:00').do(job, 'Thursday', 1)
 
     while True:
         schedule.run_pending()
