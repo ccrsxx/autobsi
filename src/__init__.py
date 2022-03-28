9580142cd1d7be275ab86fd246f542aa9a57ee1c
ccrsxx <aminrisal@gmail.com>
2022-03-28 13:46:21 +0700

    improved formatting

--- src/__init__.py
+++ src/__init__.py
@@ -3,12 +3,5 @@
 __author__ = """ccrsxx"""
 __version__ = '2.0'
 
-from .autobsi import (
-    os,
-    time,
-    logging,
-    Callable,
-    attend_class,
-    job,
-)
+from .autobsi import os, time, logging, Callable, attend_class, job
 from .environment import *
