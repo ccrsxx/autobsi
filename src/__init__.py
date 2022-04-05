6cfbbea7a4971ee58cc1a98ea0e7b6a8798cd51d
ccrsxx <aminrisal@gmail.com>
2022-04-05 20:25:24 +0700

    refactor entry point

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
-"""Automated Attendance script for MyBest BSI"""
+'''Automated Attendance script for MyBest BSI'''
 
-__author__ = """ccrsxx"""
-__version__ = '2.0'
+__author__ = '''ccrsxx'''
+__version__ = '2.1.0'
 
 from .autobsi import os, time, logging, Callable, attend_class, job
 from .environment import *
