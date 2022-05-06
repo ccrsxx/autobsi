ae233747058f0c8156165734774cb6398fc025ba
ccrsxx <aminrisal@gmail.com>
2022-05-06 20:54:47 +0700

    bump version

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
-'''Automated Attendance script for MyBest BSI'''
+'''Attendance script for MyBest BSI'''
 
 __author__ = '''ccrsxx'''
 __version__ = '2.1.0'
 
 from .autobsi import os, time, logging, Callable, attend_class, job
 from .environment import *
+from .template import *
