9d7320d622974b5035cd7d3702c47c357f6e4165
ccrsxx <aminrisal@gmail.com>
2023-10-14 16:20:00 +0700

    fix: use new selenium version to make chromedriver work (#21)

--- setup.py
+++ setup.py
@@ -1,55 +1,87 @@
 import os
+import shutil
 import platform
+import requests
 
 from win32api import GetFileVersionInfo, HIWORD
 from urllib.request import urlopen
 from zipfile import ZipFile
 from io import BytesIO
 
-from typing import cast
+from typing import TypedDict, Union, Literal, List, Dict, cast
+
 
 CHROME_PATHS = [
     os.path.join(cast(str, os.getenv(path)), 'Google\Chrome\Application\Chrome.exe')
     for path in ('ProgramFiles', 'ProgramFiles(x86)', 'LocalAppData')
 ]
 
-DRIVER_URLS = {
-    ver.split('.')[
-        0
-    ]: f'https://chromedriver.storage.googleapis.com/{ver}/chromedriver_win32.zip'
-    for ver in (
-        '90.0.4430.24',
-        '91.0.4472.101',
-        '92.0.4515.43',
-        '93.0.4577.63',
-        '94.0.4606.113',
-        '95.0.4638.69',
-        '96.0.4664.45',
-        '97.0.4692.20',
-        '98.0.4758.80',
-        '99.0.4844.17',
-        '100.0.4896.60',
-        '101.0.4951.15',
-        '102.0.5005.27',
-        '103.0.5060.134',
-        '104.0.5112.79',
-        '105.0.5195.19',
-        '106.0.5249.61',
-        '107.0.5304.62',
-        '108.0.5359.22',
-        '109.0.5414.74',
-        '110.0.5481.77',
-        '111.0.5563.64',
-        '112.0.5615.49',
-        '113.0.5672.63',
+
+class ChromeDriverDownload(TypedDict):
+    url: str
+    platform: Union[
+        Literal['linux64'],
+        Literal['mac-arm64'],
+        Literal['mac-x64'],
+        Literal['win32'],
+        Literal['win64'],
+    ]
+
+
+class ChromeMilestone(TypedDict):
+    milestone: str
+    version: str
+    revision: str
+    downloads: Dict[
+        Union[
+            Literal['chrome'], Literal['chromedriver'], Literal['chrome-headless-shell']
+        ],
+        List[ChromeDriverDownload],
+    ]
+
+
+class ChromeDriverResponse(TypedDict):
+    timestamp: str
+    milestones: Dict[str, ChromeMilestone]
+
+
+def get_chrome_driver_url(chrome_version: str) -> str:
+    response = requests.get(
+        'https://googlechromelabs.github.io/chrome-for-testing/latest-versions-per-milestone-with-downloads.json'
     )
-}
+
+    if response.status_code != 200:
+        raise Exception('Failed to get driver url.')
+
+    data: ChromeDriverResponse = response.json()
+
+    drivers = data['milestones']
+
+    filtered_drivers = {
+        version: driver
+        for version, driver in drivers.items()
+        if 'chromedriver' in driver['downloads']
+    }
+
+    if chrome_version not in filtered_drivers:
+        raise Exception(f'Your chrome version {chrome_version} is not supported.')
+
+    chrome_driver_url = None
+
+    for version, driver in filtered_drivers.items():
+        if chrome_version == version:
+            chrome_driver_url = driver['downloads']['chromedriver'][-1]['url']
+            break
+
+    if chrome_driver_url is None:
+        raise Exception(f'Failed to get driver url for version {chrome_version}.')
+
+    return chrome_driver_url
 
 
-def main() -> None:
+def get_chrome_version() -> str:
     if platform.system() != 'Windows':
-        print('This setup is only for Windows!')
-        return
+        raise Exception('This setup is only for Windows!')
 
     no_chrome = True
 
@@ -63,12 +95,16 @@ def main() -> None:
             )
 
     if no_chrome:
-        print('Google Chrome is not installed.')
-        return
+        raise Exception('Google Chrome is not installed.')
 
     if chrome_version < 90:
-        print('Google Chrome must be at least at version 90 or higher.')
-        return
+        raise Exception('Google Chrome must be at least at version 90 or higher.')
+
+    return str(chrome_version)
+
+
+def main() -> None:
+    chrome_version = get_chrome_version()
 
     print(f'Google Chrome version {chrome_version} detected.')
 
@@ -76,8 +112,13 @@ def main() -> None:
 
     print(f'Downloading webdriver for Google Chrome version {chrome_version}...')
 
-    with ZipFile(BytesIO(urlopen(DRIVER_URLS[str(chrome_version)]).read())) as zip:
+    chrome_driver_url = get_chrome_driver_url(chrome_version)
+
+    with ZipFile(BytesIO(urlopen(chrome_driver_url).read())) as zip:
         zip.extractall()
+
+    shutil.move('chromedriver-win64/chromedriver.exe', 'chromedriver.exe')
+    shutil.rmtree('chromedriver-win64')
 
     print('Setup Completed.')
 
